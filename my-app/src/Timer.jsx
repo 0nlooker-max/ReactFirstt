023cf9a66b2fdd0818462e1836ee0ca576ac5e01
Query <rexpagatpat@gmail.com>
2025-07-16 21:56:46 +0800

    minor fixes

--- my-app/src/Timer.jsx
+++ my-app/src/Timer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Row, Col, Form, Button, Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import TimerCard from "./components/timercard";
@@ -11,13 +11,50 @@ function Timer() {
   const addTimer = () => {
     const time = parseInt(inputValue);
     if (!isNaN(time) && time >= 0) {
-      setTimers([...timers, { id: Date.now(), time }]);
+      setTimers([
+        ...timers,
+        {
+          id: Date.now(),
+          timeLeft: time,
+          originalTime: time,
+          isPaused: false,
+        },
+      ]);
       setInputValue("");
     }
   };
 
-  const removeTimer = (id) => {
-    setTimers(timers.filter((t) => t.id !== id));
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimers(prevTimers =>
+        prevTimers.map(timer =>
+          !timer.isPaused && timer.timeLeft > 0
+            ? { ...timer, timeLeft: timer.timeLeft - 1 }
+            : timer
+        )
+      );
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  const removeTimer = id => {
+    setTimers(timers.filter(t => t.id !== id));
+  };
+
+  const pauseTimer = id => {
+    setTimers(timers =>
+      timers.map(t =>
+        t.id === id ? { ...t, isPaused: !t.isPaused } : t
+      )
+    );
+  };
+
+  const resetTimer = id => {
+    setTimers(timers =>
+      timers.map(t =>
+        t.id === id ? { ...t, timeLeft: t.originalTime, isPaused: false } : t
+      )
+    );
   };
 
   return (
@@ -38,11 +75,14 @@ function Timer() {
       </div>
 
       <Row className="justify-content-center gap-3">
-        {timers.map((timer) => (
+        {timers.map(timer => (
           <Col key={timer.id} xs="auto">
             <TimerCard
-              initialTime={timer.time}
-              onRemove={() => removeTimer(timer.id)}
+              timeLeft={timer.timeLeft}       
+              isPaused={timer.isPaused}      
+              onPause={() => pauseTimer(timer.id)}   
+              onReset={() => resetTimer(timer.id)}   
+              onRemove={() => removeTimer(timer.id)} 
             />
           </Col>
         ))}
